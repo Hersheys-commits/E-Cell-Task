20e90510edd06dfbb4c928730d17fb9881e643c1
Harsh Sharma <harsh1618sharma@gmail.com>
2025-01-23 18:58:42 +0530

    Reveal Animation

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../components/Header';
+import { Slide } from 'react-awesome-reveal';
 
 function ProfilePage() {
   return (
@@ -14,20 +15,22 @@ function ProfilePage() {
         >
 
           {/* Profile Info */}
-          <div className="flex flex-col items-center space-y-4 rounded-lg bg-opacity-35 bg-[#e0ac69] p-4 shadow-lg shadow-[#e0ac69]">
-            <div className="w-50 h-60 rounded-full overflow-hidden border-4 border-red-400 shadow-lg">
-              <img
-                src="/Screenshot 2024-06-06 120613.png" // Replace with your picture URL
-                alt="Your Name"
-                className="w-full h-full object-cover"
-              />
-            </div>
-            <h2 className="text-4xl font-bold" style={{ fontFamily: 'MyFont' }}>
-                Harsh Sharma
-                </h2>
+          <Slide triggerOnce>
+            <div className="flex flex-col items-center space-y-4 rounded-lg bg-opacity-35 bg-[#e0ac69] p-4 shadow-lg shadow-[#e0ac69]">
+              <div className="w-50 h-60 rounded-full overflow-hidden border-4 border-red-400 shadow-lg">
+                <img
+                  src="/Screenshot 2024-06-06 120613.png" // Replace with your picture URL
+                  alt="Your Name"
+                  className="w-full h-full object-cover"
+                />
+              </div>
+              <h2 className="text-4xl font-bold" style={{ fontFamily: 'MyFont' }}>
+                  Harsh Sharma
+                  </h2>
 
-            <p className="text-lg">Registration Number: 20233137</p>
-          </div>
+              <p className="text-lg">Registration Number: 20233137</p>
+            </div>
+          </Slide>
 
           {/* Instagram Button */}
             <a
