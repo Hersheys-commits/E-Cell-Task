5fee9642ced6572442f83035e58af459157c2c26
Harsh Sharma <harsh1618sharma@gmail.com>
2025-01-23 11:27:04 +0530

    shadow correction

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -27,8 +27,8 @@ function Home() {
     <div className="flex flex-col md:grid md:grid-cols-2 lg:flex lg:flex-row justify-center items-center gap-4 p-3 py-10">
       {/* Gradient div - full width on tablet, square otherwise */}
       <div className="w-80 h-80 md:w-full md:col-span-1 lg:w-80 order-3 md:order-1">
-  <div className="w-full h-full rounded-lg shadow-lg bg-gradient-to-br from-blue-700 to-[#FF10F0] shadow-[0_4px_8px_rgba(0,0,0,0.4)]"></div>
-</div>
+        <div className="w-full h-full rounded-lg shadow-lg bg-gradient-to-br from-blue-700 to-[#FF10F0]" style={{ boxShadow: ' 0 0px 10px 0 rgba(0, 0, 0, 0.8)'}}></div>
+      </div>
 
 
       {/* Image div - maintains square shape */}
@@ -42,7 +42,7 @@ function Home() {
       <div className="w-80 h-80 md:w-full md:h-80 lg:w-80 order-1 md:order-2">
         <div
           className="w-full h-full rounded-lg bg-gray-200 overflow-hidden p-2"
-          style={{ boxShadow: 'inset 0 1px 4px 0 rgba(0, 0, 0, 0.5)' }}
+          style={{ boxShadow: 'inset 0 0px 8px 0 rgba(0, 0, 0, 0.5)' }}
         >
           <div className="grid grid-cols-3 grid-rows-3 gap-2 w-full h-full p-5">
             {Array.from({ length: 9 }, (_, i) => (
